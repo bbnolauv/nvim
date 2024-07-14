a40b2a2bb85349e1c8df32989ac5ca73ae902ae6
bbnolauv <bbnolauv@163.com>
2024-07-15 01:23:27 +0800

    Update telescope.nvim to the latest release tag 0.1.8

--- lua/lauv/config/telescope.lua
+++ lua/lauv/config/telescope.lua
@@ -1,32 +1,19 @@
--- plugins/telescope.lua:
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.4',
-    -- or                              , branch = '0.1.x',
-    cmd = "Telescope",
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'BurntSushi/ripgrep',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  -- or                              , branch = '0.1.x',
+  cmd = "Telescope",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "BurntSushi/ripgrep",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
+  opts = {
+    extensions = {
+      fzf = { fuzzy = true, override_generic_sorter = true, override_file_sorter = true, case_mode = "smart_case" },
     },
-    config = function()
-        require('telescope').setup {
-            --          defaults = {
-            --            mappings = {
-            --              i = {
-            --                ["<C-h>"] = "which_key",
-            --              }
-            --            }
-            --          },
-            extensions = {
-                fzf = {
-                    fuzzy = true,
-                    override_generic_sorter = true,
-                    override_file_sorter = true,
-                    case_mode = "smart_case",
-                },
-            }
-        }
-        require('telescope').load_extension('fzf')
-    end,
+  },
+  init = function()
+    require("telescope").load_extension("fzf")
+  end,
 }
