87431a4024d01ef48f60ce882169d1dfa005d98b
lauv <86621528+bbnolauv@users.noreply.github.com>
2024-12-12 21:53:57 +0800

    Change nvim theme to tokyonight

--- lua/lauv/plugins/colorscheme.lua
+++ lua/lauv/plugins/colorscheme.lua
@@ -1,18 +1,11 @@
 return {
-  "ellisonleao/gruvbox.nvim",
+  "folke/tokyonight.nvim",
+  lazy = false,
   priority = 1000,
   opts = {
-    termguicolors = true,
-    guicursor = "n-v-c-sm:block,i-ci-ve:ver25-Cursor,r-cr-o:hor20",
-    overrides = {
-      -- For relative number
-      -- LineNrAbove = { fg = "#cc6666", ctermfg = "red" },
-      -- LineNrBelow = { fg = "#66cc66", ctermfg = "green" },
-      SignColumn = { bg = "none" },
-      Normal = { fg = "#ebdbb2", bg = "none" },
-    },
+    transparent = true,
   },
   init = function()
-    vim.cmd.colorscheme("gruvbox")
+    vim.cmd.colorscheme("tokyonight")
   end,
 }
