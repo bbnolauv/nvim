99562b5e58d351bfa9c224f3b420e79803dca640
lauv <86621528+bbnolauv@users.noreply.github.com>
2025-02-15 17:55:15 +0800

    Refactor init.lua to simplify entry point

--- init.lua
+++ init.lua
@@ -1,11 +1,5 @@
--- Check if running in Neovim
-if vim.fn.has("nvim") == 1 then
-  -- Require the 'lauv' module
-  require("lauv")
-else
-  -- Print error message
-  vim.api.nvim_err_writeln("Neovim hasn't been installed!!!")
-end
+vim.loader.enable()
+require("lauv")
 
 -- Check if the '.local_vimrc' file is readable
 if vim.fn.filereadable(".local_vimrc") == 1 then
