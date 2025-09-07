4f43e82455f8f6bb363fb1369a9d53a5a7238eec
lauv <86621528+bbnolauv@users.noreply.github.com>
2025-09-08 00:35:36 +0800

    chore: update plugin lockfile and add LSP inlay hint highlight

--- lua/lauv/plugins/colorscheme.lua
+++ lua/lauv/plugins/colorscheme.lua
@@ -31,6 +31,8 @@ return {
     -- vim.api.nvim_set_hl(0, "VirtualTextWarning", { link = "DiagnosticWarn" })
     -- vim.api.nvim_set_hl(0, "VirtualTextError", { link = "DiagnosticError" })
 
+    vim.api.nvim_set_hl(0, "LspInlayHint", { link = "DiagnosticVirtualTextHint" })
+
     vim.api.nvim_set_hl(0, "BlinkCmpSource", { link = "NormalFloat" })
     vim.api.nvim_set_hl(0, "BlinkCmpGhostText", { link = "Comment" })
     vim.api.nvim_set_hl(0, "TabLineFill", { bg = "NONE" })
