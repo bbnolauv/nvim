774863775813cce7ddecdbfc08fe9488879188f7
lauv <86621528+bbnolauv@users.noreply.github.com>
2025-09-13 18:22:04 +0800

    refactor(plugins): improve JSON formatter and gitsigns keymaps
    - Add fixjson formatter for JSON files
    - Replace gitsigns command strings with direct function calls
    - Comment out undo_stage_hunk mapping with explanation

--- lua/lauv/plugins/formatter.lua
+++ lua/lauv/plugins/formatter.lua
@@ -6,6 +6,7 @@ return {
     formatters_by_ft = {
       lua = { "stylua" },
       cpp = { "clang-format" },
+      json = { "fixjson" },
       -- Conform will run multiple formatters sequentially
       -- python = { "isort", "black" },
       -- Use a sub-list to run only the first available formatter
--- lua/lauv/plugins/gitsigns.lua
+++ lua/lauv/plugins/gitsigns.lua
@@ -18,7 +18,7 @@ return {
       changedelete = { text = "▎" },
     },
     on_attach = function(buffer)
-      local gs = package.loaded.gitsigns
+      local gs = require("gitsigns")
 
       local function map(mode, l, r, desc)
         vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
@@ -41,17 +41,18 @@ return {
       end, "Prev Hunk")
       map("n", "]H", function() gs.nav_hunk("last") end, "Last Hunk")
       map("n", "[H", function() gs.nav_hunk("first") end, "First Hunk")
-      map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-      map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+      map({ "n", "v" }, "<leader>ghs",gs.stage_hunk, "Stage Hunk Toggle")
+      map({ "n", "v" }, "<leader>ghr",gs.reset_hunk, "Reset Hunk")
       map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
-      map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+      -- use stage_hunk on staged_lines to undo staged_hunk
+      -- map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
       map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
       map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
       map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
       map("n", "<leader>ghB", function() gs.blame() end, "Blame Buffer")
       map("n", "<leader>ghd", gs.diffthis, "Diff This")
       map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
-      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+      map({ "o", "x" }, "ih", gs.select_hunk, "GitSigns Select Hunk")
     end,
   },
 }
