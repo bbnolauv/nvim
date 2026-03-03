29e26369e231ee0b02afcd3675f65f6602bae735
lauv <86621528+bbnolauv@users.noreply.github.com>
2026-03-04 01:05:30 +0800

    feat(theme): switch from tokyonight to catppuccin colorscheme
    - Replace tokyonight.nvim with catppuccin/nvim
    - Rename colorscheme.lua to theme.lua
    - Simplify configuration

---
+++ lua/lauv/plugins/theme.lua
@@ -0,0 +1,17 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    transparent_background = true,
+    -- float = {
+    --   transparent = true,
+    -- },
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin")
+    -- vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#878787" })
+    -- vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#276f70", italic = true })
+  end,
+}
