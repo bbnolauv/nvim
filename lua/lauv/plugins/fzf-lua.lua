85ad5b8f2486110cce744bbcdddae30433b3b65c
lauv <86621528+bbnolauv@users.noreply.github.com>
2026-03-04 02:56:14 +0800

    refactor(fzf-lua): update keybindings
    - Use <leader>f as prefix for all fzf-lua keybindings
    - Use more semantic keybindings for various actions

--- lua/lauv/plugins/fzf-lua.lua
+++ lua/lauv/plugins/fzf-lua.lua
@@ -5,24 +5,20 @@ return {
   cmd = "FzfLua",
   -- stylua: ignore start
   keys = {
-    { "<leader>u", function() FzfLua.files() end,     desc = "FzfLua files", },
-    { "<leader>o", function() FzfLua.oldfiles() end,  desc = "FzfLua oldfiles", },
-    { "<leader>k", function() FzfLua.live_grep() end, desc = "FzfLua live_grep", },
-    { "<leader>l", function() FzfLua.buffers() end,   desc = "FzfLua buffers", },
-    { "<leader>m", function() FzfLua.marks() end,     desc = "FzfLua marks", },
-    { "<leader>f", ":FzfLua ",                        desc = "Populate command line with ':FzfLua '" },
-    {
-      "<leader>i",
-      function()
-        FzfLua.lsp_document_symbols({
-          regex_filter = function(item, _)
-            local kind = item.kind
-            return kind == "Struct" or kind == "Enum" or kind == "Method" or kind == "Function"
-          end,
-        })
-      end,
-      desc = "FzfLua lsp_document_symbols",
-    },
+    { "<leader>fb", function() FzfLua.buffers() end,              desc = "FzfLua buffers", },
+    { "<leader>ff", function() FzfLua.files() end,                desc = "FzfLua files", },
+    { "<leader>fg", function() FzfLua.live_grep_native() end,     desc = "FzfLua grep", },
+    { "<leader>fh", function() FzfLua.help_tags() end,            desc = "FzfLua help_tags", },
+    { "<leader>fm", function() FzfLua.marks() end,                desc = "FzfLua marks", },
+    { "<leader>fo", function() FzfLua.oldfiles() end,             desc = "FzfLua oldfiles", },
+    { "<leader>fr", function() FzfLua.resume() end,               desc = "FzfLua resume", },
+    { "<leader>fs", function() FzfLua.lsp_document_symbols() end, desc = "FzfLua lsp_document_symbols", },
+    { "<leader>fz", function() FzfLua.zoxide() end,               desc = "FzfLua zoxide", },
+
+    { "<leader>f/", function() FzfLua.search_history() end,       desc = "FzfLua search_history", },
+    { "<leader>f:", function() FzfLua.command_history() end,      desc = "FzfLua command_history", },
+
+    { "<leader>fl", ":FzfLua ",                                   desc = "Populate command line with ':FzfLua '" },
   },
   -- stylua: ignore end
 
