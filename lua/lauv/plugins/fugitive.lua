25c43a39fcab89b39bf26ac188467321147bb207
lauv <86621528+bbnolauv@users.noreply.github.com>
2026-03-04 01:50:38 +0800

    refactor(plugins): simplify fugitive command loading
    - Remove Gread, Gwrite, Gclog from lazy loading
    - Add tip comment about Gclog compatibility

--- lua/lauv/plugins/fugitive.lua
+++ lua/lauv/plugins/fugitive.lua
@@ -1,6 +1,8 @@
 ---@type LazyPluginSpec
 return {
   "bbnolauv/vim-fugitive",
-  cmd = { "G", "Git", "Gdiffsplit", "Gread", "Gwrite", "Gclog" },
+  -- Tip: 'Gclog' doesn't work very well with lazy.nvim's on-command lazy loading.
+  -- Consider using 'FzfLua git_bcommits' instead.
+  cmd = { "G", "Git", "Gdiffsplit" },
   keys = { { "<leader>n", "<cmd>tab Git<cr>", desc = "Open fugitive" } },
 }
