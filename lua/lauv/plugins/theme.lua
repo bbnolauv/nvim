e1d40f63094f3c6b455e2a83c7f4b4646953ac9a
lauv <86621528+bbnolauv@users.noreply.github.com>
2026-03-14 13:38:17 +0800

    chore(theme): the catppuccin colorscheme was renamed upstream to catppuccin-nvim
    https://github.com/catppuccin/nvim/pull/977

--- lua/lauv/plugins/theme.lua
+++ lua/lauv/plugins/theme.lua
@@ -10,7 +10,7 @@ return {
   },
   config = function(_, opts)
     require("catppuccin").setup(opts)
-    vim.cmd.colorscheme("catppuccin")
+    vim.cmd.colorscheme("catppuccin-nvim")
     -- vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#878787" })
     -- vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#276f70", italic = true })
   end,
