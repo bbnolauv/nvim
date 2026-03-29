9fb5e4778cf7b4ce5d9036d854df317f445adf8c
lauv <86621528+bbnolauv@users.noreply.github.com>
2026-03-30 01:25:07 +0800

    refactor(keymaps): replace map wrapper with vim.keymap.set
    - Remove custom map() function that added noremap option
    - Use vim.keymap.set directly, which defaults to noremap=true
    - Preserve all existing keybindings and behavior

--- lua/lauv/config/keymaps.lua
+++ lua/lauv/config/keymaps.lua
@@ -9,49 +9,40 @@ if vim.fn.has("nvim-0.11") == 1 then
   vim.keymap.del("n", "grt")
 end
 
--- Functional wrapper for mapping custom keybindings
-local function map(mode, lhs, rhs, opts)
-  local options = { noremap = true }
-  if opts then
-    options = vim.tbl_extend("force", options, opts)
-  end
-  vim.keymap.set(mode, lhs, rhs, options)
-end
-
 -- map("i", "kj", "<Esc>", { silent = true })
-map("n", "Z", "ZZ", { silent = true })
-map(
+vim.keymap.set("n", "Z", "ZZ", { silent = true })
+vim.keymap.set(
   { "v", "n" },
   "gh",
   "(v:count == 0 || v:count == 1 ? '^^' : '^^' . (v:count - 1) . 'l')",
   { silent = true, expr = true }
 )
-map(
+vim.keymap.set(
   { "v", "n" },
   "gl",
   "(v:count == 0 || v:count == 1 ? '^$' : '^$' . (v:count - 1) . 'h')",
   { silent = true, expr = true }
 )
-map({ "v", "n", "i" }, "<F4>", "<cmd>wa<CR>")
-map({ "n", "i", "v" }, "<F1>", "<ESC><cmd>wa<CR><cmd>b#<CR>", { silent = true })
-map({ "n", "i", "v" }, "<F2>", "<ESC><cmd>wa<CR><cmd>bp<CR>", { silent = true })
-map({ "n", "i", "v" }, "<F3>", "<ESC><cmd>wa<CR><cmd>bn<CR>", { silent = true })
-map({ "n" }, "<Esc>", [[<Cmd>nohls<CR><Esc>]], { noremap = true })
+vim.keymap.set({ "v", "n", "i" }, "<F4>", "<cmd>wa<CR>")
+vim.keymap.set({ "n", "i", "v" }, "<F1>", "<ESC><cmd>wa<CR><cmd>b#<CR>", { silent = true })
+vim.keymap.set({ "n", "i", "v" }, "<F2>", "<ESC><cmd>wa<CR><cmd>bp<CR>", { silent = true })
+vim.keymap.set({ "n", "i", "v" }, "<F3>", "<ESC><cmd>wa<CR><cmd>bn<CR>", { silent = true })
+vim.keymap.set({ "n" }, "<Esc>", [[<Cmd>nohls<CR><Esc>]], { noremap = true })
 
-map({ "i" }, "<c-l>", [[<Right>]])
+vim.keymap.set({ "i" }, "<c-l>", [[<Right>]])
 
 -- windows movement
-map({ "v", "n", "t" }, "<C-h>", [[<C-w>h]])
-map({ "v", "n", "t" }, "<C-j>", [[<C-w>j]])
-map({ "v", "n", "t" }, "<C-k>", [[<C-w>k]])
-map({ "v", "n", "t" }, "<C-l>", [[<C-w>l]])
+vim.keymap.set({ "v", "n", "t" }, "<C-h>", [[<C-w>h]])
+vim.keymap.set({ "v", "n", "t" }, "<C-j>", [[<C-w>j]])
+vim.keymap.set({ "v", "n", "t" }, "<C-k>", [[<C-w>k]])
+vim.keymap.set({ "v", "n", "t" }, "<C-l>", [[<C-w>l]])
 
 ---@type Utils.runner
 local UtilRunner = require("lauv.utils.runner")
 
-map("n", "<F5>", UtilRunner.build_and_run, { silent = true })
-map("n", "<F6>", UtilRunner.run, { silent = true })
-map("n", "<leader>gg", UtilRunner.lazygit, { silent = true })
+vim.keymap.set("n", "<F5>", UtilRunner.build_and_run, { silent = true })
+vim.keymap.set("n", "<F6>", UtilRunner.run, { silent = true })
+vim.keymap.set("n", "<leader>gg", UtilRunner.lazygit, { silent = true })
 
 vim.api.nvim_create_user_command("Diagnostics", function(opts)
   local qf_info = vim.fn.getqflist({ title = 1, winid = 1 })
@@ -69,4 +60,4 @@ vim.api.nvim_create_user_command("Diagnostics", function(opts)
   end
 end, { bang = true, desc = "Toggle diagnostics in quickfix list" })
 
-map("n", "<leader>t", "<cmd>Diagnostics<cr>", { silent = true })
+vim.keymap.set("n", "<leader>t", "<cmd>Diagnostics<cr>", { silent = true })
