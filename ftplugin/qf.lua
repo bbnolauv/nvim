7306b33d86ce1116eb911f522887f12ea630b15c
lauv <86621528+bbnolauv@users.noreply.github.com>
2026-03-30 01:32:33 +0800

    refactor(qf): simplify quickfix ftplugin
    - Remove local variable wrappers for api, keymap, bufnr, winid
    - Use vim.wo.winfixbuf directly instead of vim.wo[winid][0]
    - Add nowait to q close keymap

--- ftplugin/qf.lua
+++ ftplugin/qf.lua
@@ -1,10 +1,4 @@
-local api = vim.api
-local keymap = vim.keymap
-
-local bufnr = api.nvim_get_current_buf()
-local winid = api.nvim_get_current_win()
-
 -- Avoid opening other buffers in the quickfix window.
-vim.wo[winid][0].winfixbuf = true
+vim.wo.winfixbuf = true
 
-keymap.set("n", "q", "<Cmd>close<CR>", { desc = "Close Quickfix", buffer = bufnr })
+vim.keymap.set("n", "q", "<Cmd>close<CR>", { desc = "Close Quickfix", nowait = true, buffer = true })
