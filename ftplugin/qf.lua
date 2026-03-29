802555e57df1a36940070e97a71c9df74423593c
lauv <86621528+bbnolauv@users.noreply.github.com>
2026-03-30 01:32:43 +0800

    feat(qf): add R keymap for cdo substitute
    - Add R keymap in quickfix buffer to run :cdo s/// for each entry

--- ftplugin/qf.lua
+++ ftplugin/qf.lua
@@ -2,3 +2,9 @@
 vim.wo.winfixbuf = true
 
 vim.keymap.set("n", "q", "<Cmd>close<CR>", { desc = "Close Quickfix", nowait = true, buffer = true })
+vim.keymap.set(
+  "n",
+  "R",
+  ':cdo s/<c-r>"//gc<Left><Left><Left>',
+  { desc = "Run substitute for each entry", buffer = true }
+)
