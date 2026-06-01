11265cd3589383fa590d8eb86a44c7e2218fec49
lauv <86621528+bbnolauv@users.noreply.github.com>
2026-06-01 22:03:34 +0800

    feat: add barrettruth/diffs.nvim

---
+++ lua/lauv/plugins/diff.lua
@@ -0,0 +1,11 @@
+return {
+  'barrettruth/diffs.nvim',
+  config = function()
+    vim.g.diffs = {
+      integrations = {
+        fugitive = true,
+        gitsigns = true,
+      },
+    }
+  end,
+}
