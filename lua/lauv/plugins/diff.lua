5970aea3534bac537065c62c4afb45613d87e63f
lauv <86621528+bbnolauv@users.noreply.github.com>
2026-06-27 17:21:48 +0800

    feat: add dlyongemallo/diffview-plus.nvim

--- lua/lauv/plugins/diff.lua
+++ lua/lauv/plugins/diff.lua
@@ -1,11 +1,26 @@
 return {
-  'barrettruth/diffs.nvim',
-  config = function()
-    vim.g.diffs = {
-      integrations = {
-        fugitive = true,
-        gitsigns = true,
-      },
-    }
-  end,
+  -- Lazy
+  {
+    'dlyongemallo/diffview-plus.nvim',
+    version = '*',
+    -- optional: lazy-load on command
+    cmd = {
+      'DiffviewOpen',
+      'DiffviewToggle',
+      'DiffviewFileHistory',
+      'DiffviewDiffFiles',
+      'DiffviewLog',
+    },
+  },
+  {
+    'barrettruth/diffs.nvim',
+    config = function()
+      vim.g.diffs = {
+        integrations = {
+          fugitive = true,
+          gitsigns = true,
+        },
+      }
+    end,
+  },
 }
